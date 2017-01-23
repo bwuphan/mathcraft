c07f4ca5e27becc5c4b69003a9eb9060f3614f20
bobby <bwuphan@gmail.com>
2017-01-23 13:11:51 -0800

    capitalized PORT

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var app = express();
 //   console.log('hello')
 //   res.send('Hello world')
 // })
-var port = process.env.port || 8000
+var port = process.env.PORT || 8000
 app.use(express.static(__dirname + '/../client'));
 app.listen(port, function() {
   console.log('listening on port', port)
