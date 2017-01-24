93e62dcb3370a8f59b810eb159e469431e891b78
bobby <bwuphan@gmail.com>
2017-01-24 08:49:28 -0800

    uncommented out mongoose in server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
-// var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 var userController = require('./users/userController.js');
 var bodyParser = require('body-parser');
 app.use(bodyParser());
 
 //mongod uri mongodb://heroku_x47wsjz1:r76ua1mhvk2d4s9h77n8b3tkts@ds129469.mlab.com:29469/heroku_x47wsjz1
 
-// mongoose.connect('mongodb://localhost/mathcraft')
+mongoose.connect('mongodb://heroku_x47wsjz1:r76ua1mhvk2d4s9h77n8b3tkts@ds129469.mlab.com:29469/heroku_x47wsjz1')
 
-// var db = mongoose.connection;
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('we connected')
