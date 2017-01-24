b60ff7ed11605b7082322396e4255f7314ef5a1c
bobby <bwuphan@gmail.com>
2017-01-23 16:22:27 -0800

    added timer function

--- client/app.js
+++ client/app.js
@@ -1,7 +1,27 @@
 angular.module('mathApp',[])
-.controller('MathController', function($scope, $location) {
+.controller('MathController', function($scope, $location, $timeout) {
   $scope.usrAnswer = '';
+  $scope.time = 60;
 
+  //Timer function
+  $scope.timerStart = function() {
+    $scope.time--;
+    $timeout($scope.timerStart, 1000);
+
+    console.log($scope.time)
+  }
+
+  // $scope.counter = 0;
+  //   $scope.onTimeout = function(){
+  //       $scope.counter++;
+  //       mytimeout = $timeout($scope.onTimeout,1000);
+  //   }
+  //   var mytimeout = $timeout($scope.onTimeout,1000);
+
+  //   $scope.stop = function(){
+  //       $timeout.cancel(mytimeout);
+  //   }
+  // }
   // Generates random operator
   $scope.randomOp = function() {
     var randNum = Math.random();
@@ -12,13 +32,12 @@ angular.module('mathApp',[])
     }
   }
 
+
   //initialized random expression
   $scope.expression = `${Math.floor(Math.random() * 12)} ${$scope.randomOp()} ${Math.floor(Math.random() * 12)}`
   $scope.check = true
   //Generates random expresssion
   $scope.random = function() {
-    console.log($scope.answer)
-    console.log(eval($scope.expression));
     $scope.usrAnswer == eval($scope.expression) ? $scope.check = 'true': $scope.check = 'false'
     $scope.expression = `${Math.floor(Math.random() * 12)} ${$scope.randomOp()} ${Math.floor(Math.random() * 12)}`
     $scope.answer = '';
