eff01d4ef14fad7e828b87dc4705a2d4bdeec929
bobby <bwuphan@gmail.com>
2017-01-23 16:53:42 -0800

    added a increment and decrement score

--- client/app.js
+++ client/app.js
@@ -1,27 +1,28 @@
 angular.module('mathApp',[])
 .controller('MathController', function($scope, $location, $timeout) {
   $scope.usrAnswer = '';
-  $scope.time = 60;
-
+  $scope.time = 10;
+  $scope.score = 0;
+  $scope.start = false;
   //Timer function
   $scope.timerStart = function() {
-    $scope.time--;
-    $timeout($scope.timerStart, 1000);
-
-    console.log($scope.time)
+    if($scope.time !== 0){
+      $scope.start = $scope.start || true;
+      $scope.time--;
+      $timeout($scope.timerStart, 1000);
+    } else {
+      $scope.start = false;
+    }
   }
 
-  // $scope.counter = 0;
-  //   $scope.onTimeout = function(){
-  //       $scope.counter++;
-  //       mytimeout = $timeout($scope.onTimeout,1000);
-  //   }
-  //   var mytimeout = $timeout($scope.onTimeout,1000);
+  $scope.incScore = function() {
+    $scope.score += 100;
+  }
 
-  //   $scope.stop = function(){
-  //       $timeout.cancel(mytimeout);
-  //   }
-  // }
+  $scope.decScore = function() {
+    $scope.score -= 100;
+  }
+
   // Generates random operator
   $scope.randomOp = function() {
     var randNum = Math.random();
@@ -32,13 +33,14 @@ angular.module('mathApp',[])
     }
   }
 
-
   //initialized random expression
   $scope.expression = `${Math.floor(Math.random() * 12)} ${$scope.randomOp()} ${Math.floor(Math.random() * 12)}`
   $scope.check = true
   //Generates random expresssion
   $scope.random = function() {
-    $scope.usrAnswer == eval($scope.expression) ? $scope.check = 'true': $scope.check = 'false'
+    console.log('in here')
+    $scope.usrAnswer == eval($scope.expression) ? $scope.check = true : $scope.check = false;
+    $scope.check === true ? $scope.incScore() : $scope.decScore();
     $scope.expression = `${Math.floor(Math.random() * 12)} ${$scope.randomOp()} ${Math.floor(Math.random() * 12)}`
     $scope.answer = '';
   }
